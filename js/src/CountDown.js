9a0c271f1c0bc4d7b4fb91a1d9118d436ff70928
birdbird314 <grantstadt@gmail.com>
2018-03-25 16:19:34 +0200

    Remove unnecessary flag

--- js/src/CountDown.js
+++ js/src/CountDown.js
@@ -2,22 +2,19 @@ function CountDown(time, timerElement) {
   var time = time;
   var timerElement = timerElement;
 
-  var interrupted;
   var timeout;
 
   this.start = function() {
-    interrupted = false;
     countDown(time);
   };
 
   this.stop = function() {
-    interrupted = true;
     window.clearTimeout(timeout);
   }
 
   function countDown(time) {
     time.printTo(timer);
-    if (time.isNonZero() && !interrupted) {
+    if (time.isNonZero()) {
       timeout = window.setTimeout(
         () => countDown(time.minusOneSecond()),
         1000
