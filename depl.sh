9714b72083e51976e8480fe59f44611993c71dd2
birdbird314 <grantstadt@gmail.com>
2018-03-25 18:49:04 +0200

    Fix js files paths

--- depl.sh
+++ depl.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# Clear docs dir
 rm -r docs/*
+
+# Copy html and css files
 cp pomodoro.html docs/index.html
 cp -r css docs/
+
+# Copy js files
 mkdir docs/js
-cp -r js/src/* docs/js/
+mkdir docs/js/src
+cp -r js/src/* docs/js/src/
+
+# Remove mocked scripts
 rm docs/*/*Mock*
+
 echo "INFO: Done"
